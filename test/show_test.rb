1f762531d9cb36cda2dbb1bbd46dcbdf766da666
javolpe <volpe.jacob@gmail.com>
2020-12-15 09:26:58 -0700

    Write tests for iteration 2

--- test/show_test.rb
+++ test/show_test.rb
@@ -29,4 +29,35 @@ class ShowTest < Minitest::Test
     assert_equal "Glen Larson", knight_rider.creator
   end
 
+  def test_it_has_characters
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal [michael_knight, kitt], knight_rider.characters
+  end
+
+  def test_total_salary_of_show
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
+
+  def test_highest_paid_actor
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
+  end
+
+  def test_actors_as_strings_in_array
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal ["David Hasselhoff", "William Daniels"], knight_rider.actors
+  end
 end
