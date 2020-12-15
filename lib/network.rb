529a4d1efed2eb6fbc0a5b016741307508e8b9d5
javolpe <volpe.jacob@gmail.com>
2020-12-15 10:18:50 -0700

    Write test and helper test for actors by show

--- lib/network.rb
+++ lib/network.rb
@@ -8,5 +8,31 @@ class Network
     @shows = []
   end
 
+  def add_show(show)
+    shows << show
+  end
+
+  def characters_in_upcase
+    array = []
+    shows.each do |show|
+      show.characters.each do |character|
+        if character.name === character.name.upcase
+        array << character
+        end
+      end
+    end
+    array
+  end
+
+  def main_characters
+    mc_array = []
+    characters_in_upcase.each do |character|
+      if  character.salary > 500_000
+        mc_array << character
+      end
+    end
+    mc_array
+  end
+
 
 end
