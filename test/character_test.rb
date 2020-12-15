34e16e6704695ad77773690d8e0ebb4895282ab9
javolpe <volpe.jacob@gmail.com>
2020-12-15 09:06:35 -0700

    Initial commit, first two tests

---
+++ test/character_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_instance_of Character, kitt
+  end
+
+  def test_it_has_a_name
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT", kitt.name
+  end
+
+end
